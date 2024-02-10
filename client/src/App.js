9714608ece1d8582a88a6ab6a9694bc866fb77e9
Max Shilo <maxshilo@maxmacair.local>
2024-02-10 20:52:11 +0200

    added mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,15 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+import User from './components/User'
+
 function App() {
   const [data, setData] = React.useState(null)
+  const [user, setUser] = React.useState(null)
+  const [users, setUsers] = React.useState(null)
+
+  const [username, setUsername] = React.useState(null)
+  const [password, setPassword] = React.useState(null)
 
   React.useEffect(() => {
     fetch('/api')
@@ -11,11 +18,56 @@ function App() {
       .then((data) => setData(data.message))
   }, [])
 
+  const getUser = async () => {
+    await fetch('/dbgetUser')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }
+
+  const registerUser = async () => {
+    let newUser = {
+      username,
+      password
+    }
+
+    await fetch('/registerUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => alert(data.added))
+  }
+
+  const showAllUsers = async () => {
+    let tempUsers = []
+    tempUsers = await fetch('/getAllUsers').then(res => res.json())
+    setUsers([...tempUsers])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading ..." : data}</p>
+
+        {/* <p>{!data ? "Loading ..." : data}</p> */}
+        {/* <p>{!user ? 'click button to show user' : user.username}</p> */}
+        {/* <button onClick={() => getUser()}>CLICK ME</button> */}
+
+        <div className='container'>
+          <input onChange={(e) => { setUsername(e.target.value)}} type='text' placeholder='username'></input>
+          <input onChange={(e) => { setPassword(e.target.value)}} type='password' placeholder='password'></input>
+          <button onClick={() => registerUser()}>REGISTER</button>
+        </div>
+
+        <div className='container'>
+          <button onClick={() => {showAllUsers()}}>SHOW USERS</button>
+          {
+            ! users ? 'no users to show ...' : users.map(user => <User {...user} />)
+          }
+        </div>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,18 +1,55 @@
 const path = require('path')
 const express = require('express')
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3001
-
 const app = express()
+
 app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb+srv://maximshilo00:326974359max@projectdb.j3ljufi.mongodb.net/reacttest')
+
+const userSchema = mongoose.Schema({
+    username : String,
+    password : String
+})
+
+const usersModel = mongoose.model('users', userSchema)
 
 app.get('/api', (req, res) => {
     res.json({ message : 'I LOVE YOU MIKI!'})
 })
 
+app.get('/dbgetUser', async (req, res) => {
+    let tempUser = await usersModel.findOne()
+    res.json(tempUser)
+})
+
+app.get('/getAllUsers', async (req, res) => {
+    let tempUsers = await usersModel.find()
+    res.send(tempUsers)
+})
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+
+app.post('/registerUser', async (req, res) => {
+    let tempUser = req.body
+    await usersModel.insertMany(tempUser)
+
+    tempUser = null
+    tempUser = await usersModel.findOne({ username : req.body.username })
+
+    if (tempUser) {
+        res.json({ added : 'true'})
+    } else {
+        res.json({ added : 'false'})
+    }
 })
 
 app.listen(PORT, () => {
